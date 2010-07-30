a83da271e9b52202b2a324a458c3590362d4a42f
Ngoc Dao <ngoc@ngoc-2.local>
2010-07-31 01:44:21 +0900

    Update to Clojure RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject vsccpc "0.1-SNAPSHOT"
   :description "Clojure implementation of VSCCP"
-  :dependencies [[org.clojure/clojure         "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure         "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]])
